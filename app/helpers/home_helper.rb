141d4aa50cdbfc4ffdddb0fa5e05af3a84bdf19c
Adam Garstka <adam_garstka@yahoo.com>
2025-11-08 17:49:49 +0100

    rearrange index

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def playlist_button_attributes
+    classes = %w[ btn btn-dark ]
+    classes << "text-decoration-line-through" unless spotify_user_authenticated?
+
+    attributes = Hash.new
+    attributes[:class] = classes.join(" ")
+    attributes[:disabled] = !spotify_user_authenticated?
+    attributes[:type] =  "button"
+    attributes
+  end
 end
